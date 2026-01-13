16d61844969def4afc567b23438e10466dafdda6
Abanoub Youssef <abanoub.youssef@scandiweb.com>
2026-01-13 14:04:07 +0200

    CMA Docker setup files

---
+++ cma.js
@@ -0,0 +1,19 @@
+/** @type {import('@scandipwa/magento-scripts').CMAConfiguration} */
+module.exports = {
+    magento: {
+        first_name: 'Abanoub',
+        last_name: 'Youssef',
+        email: 'web.developer@gila-electric.com',
+        user: 'admin',
+        password: 'gila123',
+        adminuri: 'admin',
+        mode: 'developer',
+        edition: 'community'
+    },
+    configuration: {
+        searchengine: 'opensearch'
+    },
+    storeDomains: {
+        admin: "gila.local",
+    },
+}
